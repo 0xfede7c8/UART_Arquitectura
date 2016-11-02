c793199d30e4be6c23ea0e40a6478e8371b35e60
PerezFederico <perezfederico@unc.edu.ar>
2016-11-02 19:33:18 -0300

    caca

--- ALU.v
+++ ALU.v
@@ -1,20 +1,20 @@
 `timescale 1ns / 1ps
 
 module ALU 
-	#(parameter bus=8)
-	(input signed [bus-1:0]A, input signed [bus-1:0]B,input [5:0]Opcode,
+	#(parameter bus=32)
+	(input signed [bus-1:0]A, input signed [bus-1:0]B,input [7:0]Opcode,
 	output reg[bus-1:0]Out);
 	always@(*)
 	begin
 		case (Opcode)
-			'b100000: Out=A+B; //ADD
-			'b100010: Out=A-B; //SUB
-			'b100100: Out=A&B; //AND
-			'b100101: Out=A|B; //OR
-			'b100110: Out=A^B; //XOR
-			'b100111: Out=~(A|B); //NOR
-			'b000011: Out=A>>>B; //SRA
-			'b000010: Out=A>>B; //SRL
+			8'b00100000: Out=A+B; //ADD
+			8'b00100010: Out=A-B; //SUB
+			8'b00100100: Out=A&B; //AND
+			8'b00100101: Out=A|B; //OR
+			8'b00100110: Out=A^B; //XOR
+			8'b00100111: Out=~(A|B); //NOR
+			8'b00000011: Out=A>>>B; //SRA
+			8'b00000010: Out=A>>B; //SRL
 			default: Out= 0;
 		endcase
 	end
